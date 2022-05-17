be06a3a6341bfefb06e7064295c41f88dda76ba8
Sander Saares <sasaares@microsoft.com>
2022-05-17 14:20:59 +0800

    I meant time.windows.com

--- Common/Constants.cs
+++ Common/Constants.cs
@@ -2,7 +2,7 @@
 
 public static class Constants
 {
-    public static readonly Uri TimeserverUrl = new Uri("ntp://time.microsoft.com");
+    public static readonly Uri TimeserverUrl = new Uri("ntp://time.windows.com");
 
     public const int MediaSegmentDurationSeconds = 2;
 }
